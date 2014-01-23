15ff41788100a29900c98582e11be5c2f9316234
shanmugavel <iashanmugavel@gmail.com>
2014-01-23 19:40:29 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarrierwaveExample::Application.config.secret_key_base = 'c1ea317309b3b824e38bc0ac5d25f3d3a583bce3f38d3e6cadd2ee0aa28c8c67757b426b594e714a541217de30f98b08f8bbfd2b3015edbbafe412c8b8f11ea7'
